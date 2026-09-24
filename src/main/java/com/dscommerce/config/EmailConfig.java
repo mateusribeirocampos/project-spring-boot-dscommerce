cc6d4641b73797e4ef48e2a9b2b39d5de66a4e90
mateusribeirocampos <mateusrcampos@gmail.com>
2026-09-24 17:37:21 -0300

    Fixes: emailTaskExecutor, @EnableAsync, and EmailResendService - emailTaskExecutor and @EnableAsync have been removed from EmailConfig. - EmailResendService now throws a RuntimeException within the catch block of the plainTextEmail method if the resend fails. - RabbitMQ enables asynchronous email sending containing purchase details after the user places an order.fix:

--- src/main/java/com/dscommerce/config/EmailConfig.java
+++ src/main/java/com/dscommerce/config/EmailConfig.java
@@ -4,16 +4,9 @@ import com.dscommerce.services.email.EmailService;
 import com.dscommerce.services.email.ResendEmailService;
 import com.resend.Resend;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.util.concurrent.Executor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.context.annotation.*;
 
 @Configuration
-@EnableAsync
 @Profile({"dev", "prod"})
 public class EmailConfig {
 
@@ -28,16 +21,5 @@ public class EmailConfig {
     @Bean
     public Resend resend() {
         return new Resend(apiKey);
-    }
-
-    @Bean(name = "emailTaskExecutor")
-    public Executor emailTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2); //thread sempre ativas
-        executor.setMaxPoolSize(5); //maximo em pico
-        executor.setQueueCapacity(100);
-        executor.setThreadNamePrefix("email-");
-        executor.initialize();
-        return executor;
     }
 }
